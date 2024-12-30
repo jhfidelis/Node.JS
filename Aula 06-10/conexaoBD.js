5dc1818af1fe0431a926253e6142b4a48184be05
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-12-29 23:28:45 -0300

    #18 - Criando models no Sequelize

--- Aula 06-10/conexaoBD.js
+++ Aula 06-10/conexaoBD.js
@@ -12,3 +12,50 @@ sequelize.authenticate().then(function() {
 }).catch(function(erro) {
     console.log("Falha ao se conectar: " + erro);
 });
+
+// Criando a tabela de postagens
+const Postagem = sequelize.define(/*nome da tabela*/'postagens', /*campos da tabela*/ {
+    titulo: { // Nome do atributo
+        type: Sequelize.STRING // Tipo do atributo
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+});
+
+// Gerar um model diretamente no MySQL
+Postagem.sync(/*Obj para garantir a geração da tabela*/{force: true}); // Função que sincroniza o model com o MySQL
+/* Sempre comentar ou apagar o comando para que ele seja executado apenas 1 vez */
+
+// Criando um registro na tabela de postagem
+Postagem.create({
+    titulo: "Título da postagem",
+    conteudo: "Conteúdo da postagem que está sendo criada"
+}); // Sempre comentar ou apagar o comando para que ele seja executado apenas 1 vez
+
+// Criando model para usuários
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+// Sincronizando o model com o BD
+Usuario.sync({force: true});
+
+// Criando um registro na tabela de usuários
+Usuario.create({
+    nome: "Henrique",
+    sobrenome: "Fidelis",
+    idade: 20,
+    email: "fidelis.henrique@gmail.com"
+});
