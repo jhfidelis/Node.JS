34dff3c87839fb27f140e6f9955499a10bdd2f22
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-01-03 23:39:24 -0300

    #20 - Criando um formulário

--- Projeto 02/index.js
+++ Projeto 02/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars')// Constante que vai receber o módulo express-handlebars
+const handlebars = require('express-handlebars');// Constante que vai receber o módulo express-handlebars
 const Sequelize = require('sequelize');
 
 // Config
     // Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
     app.set('view engine', 'handlebars');
 
     // Conexão com o banco de dados MySWL
@@ -14,6 +14,11 @@ const Sequelize = require('sequelize');
         dialect: 'mysql'
     });
 
+// Criando formulário de cadastro de posts
+    // Rotas
+    app.get('/cadastro', function(req, res) {
+        res.render('formulario'); // Método que vai "chamar" o arquivo que será exibido
+    });
 
 app.listen(8081, function() {
     console.log("Servidor rodando na URL http://localhost:8081");
