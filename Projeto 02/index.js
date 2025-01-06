5c8c354bc98895fa52b63896129290b102896a97
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-01-06 18:48:46 -0300

    #24 - Recebendo os dados do formulário

--- Projeto 02/index.js
+++ Projeto 02/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');// Constante que vai receber o módulo express-handlebars
 const bodyParser = require('body-parser'); //Constante que vai receber o módulo body-parser
+const Post = require('./models/Post')// Constnte que recebe o model Post para ser manipulado por esse arquivo
 
 // Config
     // Template Engine
@@ -13,6 +14,12 @@ const bodyParser = require('body-parser'); //Constante que vai receber o módulo
     app.use(bodyParser.json());
 
 // Rotas
+
+    // Rota principal do sistema que retornará para a 'home'
+    app.get('/', function(req, res) {
+        res.render('home');
+    })
+
     // Criando formulário de cadastro de posts
     app.get('/cadastro', function(req, res) {
         res.render('formulario'); // Método que vai "chamar" o arquivo que será exibido
@@ -20,9 +27,15 @@ const bodyParser = require('body-parser'); //Constante que vai receber o módulo
 
     // Criando rota para enviar os dados do formulário
     app.post('/enviarForm', function(req, res) {
-        var texto1 = req.body.titulo;
-        var texto2 = req.body.conteudo;
-        res.send("Titulo: " + texto1 + " Conteudo: " + texto2);
+        //Criando uma instância de Post
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function() { // Verifica se a instância foi criada
+            res.redirect('/');
+        }).catch(function(erro) { // função que retorna o erro no proceso de criação
+            res.send("Houve um erro: " + erro);
+        });
     });
 
 app.listen(8081, function() {
